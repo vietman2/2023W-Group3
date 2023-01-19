0168c78e2aa5355316d7394002cd4cb8e3ecf1dc
vietman2 <vietman0@gmail.com>
2023-01-19 19:03:56 +0900

    lint retry

--- app/App.tsx
+++ app/App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>retry!</Text>
+      <Text>try again!</Text>
       <StatusBar style="auto" />
     </View>
   );
