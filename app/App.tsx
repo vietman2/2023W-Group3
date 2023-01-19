bed47ff94536a829f3e59a05270b52d6c665f297
vietman2 <vietman0@gmail.com>
2023-01-19 18:58:47 +0900

    test lint

--- app/App.tsx
+++ app/App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>test!</Text>
+      <Text>retry!</Text>
       <StatusBar style="auto" />
     </View>
   );
